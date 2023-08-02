7764df810a1792012dd98b634400cd2d212e55fd
ChangeSuger <824737351@qq.com>
2023-08-02 22:45:28 +0800

    feat: code type suggestion for code block

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 这是绘开发，绘自用的 Obsidian 插件，包含绘常用的功能：
 
-- 自动更新 YAML Front-matter
+- YAML Front-matter
+  - 快捷键更新 YAML Front-matter 的部分信息
+- 代码块
+  - 代码类型提示
 
 TODO:
 
-- Setting 面板
-- Modal 批量编辑 YAML Front-matter
+- Setting 面板完善
+- YAML Front-matter
+  - Modal 批量编辑 YAML Front-matter
+- 代码类型列表补全
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import {
 	Editor,
 	MarkdownView,
 	Plugin,
+	EditorSuggest,
+	EditorPosition,
+	EditorSuggestTriggerInfo,
+	TFile,
+	EditorSuggestContext,
 } from 'obsidian';
 import {
 	CamillePluginSettings,
@@ -12,11 +17,14 @@ import { updateFrontMatter } from './yamlFrontMatter';
 
 import { test } from './test';
 
+const codeTypeList = require('./codeTypeList.json') as string[];
+
 export default class CamillePlugin extends Plugin {
 	settings: CamillePluginSettings;
 
 	async onload () {
 		await this.loadSettings();
+		this.registerEditorSuggest(new CodeTypeSuggest(this));
 
 		this.addCommand({
 			id: 'yaml-front-matter-update',
@@ -45,5 +53,53 @@ export default class CamillePlugin extends Plugin {
 
 	async saveSettings () {
 		await this.saveData(this.settings);
+	}
+}
+
+class CodeTypeSuggest extends EditorSuggest<string> {
+	plugins: CamillePlugin;
+	codeTypeList: string[];
+
+	constructor (plugins: CamillePlugin) {
+		super(plugins.app);
+		this.plugins = plugins;
+		this.codeTypeList = codeTypeList;
+	}
+
+	onTrigger(cursor: EditorPosition, editor: Editor, _: TFile | null): EditorSuggestTriggerInfo | null {
+		const line = editor.getLine(cursor.line);
+		const match = line.match(/```(.*)/)?.first();
+		if (match) {
+			return {
+				start: {
+					ch: line.lastIndexOf(match),
+					line: cursor.line,
+				},
+				end: cursor,
+				query: match,
+			};
+		}
+		return null;
+	}
+
+	getSuggestions (context: EditorSuggestContext): string[] {
+		let codeTypeQuery = context.query
+			.replace(/```/, '')
+			.toLowerCase();
+		return this.codeTypeList
+			.filter(codeType => codeType.includes(codeTypeQuery));
+	}
+
+	renderSuggestion(value: string, el: HTMLElement): void {
+		const suggestions = el.createDiv({ cls: "code-type-suggester-container" });
+		suggestions
+			.createDiv({ cls: "code-type-suggester-item" })
+			.setText(value);
+	}
+
+	selectSuggestion(value: string): void {
+		if (this.context) {
+			this.context.editor.replaceRange(`\`\`\`${value} `, this.context.start, this.context.end);
+		}
 	}
 }
