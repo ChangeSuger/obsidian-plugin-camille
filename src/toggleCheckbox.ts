6d58202300cfff873b3a58b5e6649b41d2c972e2
ChangeSuger <824737351@qq.com>
2023-10-19 19:25:46 +0800

    fix(toggleCheckbox): fix uncorrect cursor section position after toggle

--- src/toggleCheckbox.ts
+++ src/toggleCheckbox.ts
@@ -6,10 +6,20 @@ const CHECKBOX_REGEX = /(?<prefix>^\s*-\s)(?<checkbox>(\[(\s|x)\]\s)?)(?<content
 
 export function toggleCheckboxs (editor: Editor) {
   const [from, to] = [editor.getCursor("from"), editor.getCursor("to")];
+  const [fromLine, toLine] = [editor.getLine(from.line), editor.getLine(to.line)];
   for (let line = from.line; line <= to.line; line++) {
     toggleCheckboxByLine(editor, line);
   }
-  editor.setSelection(from, to);
+  const [newFromLine, newToLine] = [editor.getLine(from.line), editor.getLine(to.line)];
+  editor.setSelection(
+    {
+      ...from,
+      ch: from.ch + (newFromLine.length - fromLine.length)
+    }, {
+      ...to,
+      ch: to.ch + (newToLine.length - toLine.length)
+    }
+  );
 }
 
 function toggleCheckboxByLine(editor: Editor, cursorLine: number) {
